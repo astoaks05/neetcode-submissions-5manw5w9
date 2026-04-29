57b8fb76bbcef6d7cb74ab8a95149a5026b9f9e2
Aidan Stoaks <144216766+astoaks05@users.noreply.github.com>
2026-04-29 10:30:24 -0600

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        return_val = [0, 0]
+        # what should keys and values be? 
+        # key should be index, value should be value 
+        for i in range (len(nums)):
+            if nums[i] not in hashmap:
+                hashmap[nums[i]] = i
+            difference = target - nums[i]
+            if difference in hashmap:
+                return_val[0] = hashmap[difference]
+                return_val[1] = i
+        return return_val
+                    
+
